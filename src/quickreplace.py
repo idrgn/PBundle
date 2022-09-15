9ab3cd265618eb6198fd19af3c897a941a87f139
Maikel <maikel@local.com>
2022-09-15 09:32:24 +0200

    Accidentally deleted quickreplace

---
+++ src/quickreplace.py
@@ -0,0 +1,54 @@
+from genericpath import exists
+
+from bnd.bnd import BND
+
+
+def quick_replace(bnd_file_path, source_file_path, destination):
+    """_summary_
+    Args:
+        bnd_file_path (_type_): _description_
+        source_file_path (_type_): _description_
+        destination (_type_): _description_
+    """
+    print(
+        "Replacing file", destination, "inside", bnd_file_path, "with", source_file_path
+    )
+
+    # Check if replacement file exists
+    if not exists(source_file_path):
+        print("Source file", source_file_path, "doesn't exist")
+        return
+
+    # Check if BND file exists
+    if not exists(bnd_file_path):
+        print("BND file", bnd_file_path, "doesn't exist")
+        return
+
+    # Check if destination file is valid
+    if destination.endswith("/"):
+        print("Destination cannot be a folder")
+        return
+
+    # Do stuff
+    with open(bnd_file_path, "r+b") as f:
+        data = f.read()
+        bnd_file = BND(data)
+        destination_array = destination.split("/")
+        destination_file = bnd_file.get_bundle_from_filename_array(destination_array)
+
+        # Return if destination file not found
+        if destination_file == None:
+            print("Couldn't find destination file")
+            return
+
+        # Replace
+        with open(source_file_path, "r+b") as s:
+            source_data = s.read()
+            destination_file.update_data(source_data)
+
+        # Return to bytes
+        data = bnd_file.to_bytes()
+
+    # Write bytes
+    with open(bnd_file_path, "wb") as f:
+        f.write(data)
